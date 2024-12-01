fdebb2cd47e8bf0eb7f9f9e238352de1c93b427f
mt.khademi <Mohande3khademi@gmail.com>
2024-12-01 09:06:28 +0330

    Data type : integer and string

--- learningKotlin/src/Main.kt
+++ learningKotlin/src/Main.kt
@@ -1,3 +1,21 @@
 fun main() {
-    println("Hello World!")
+
+    var userName = "John"
+    userName = "New name john"
+
+    val family = "New family"
+    // family = "changed" // error
+
+    var age: Int = 21 // only number
+    // var age = 20 -> type inference -> type of age => Integer
+    age = 25
+
+//    age = "Alex" // Get error -> hold only integer
+
+    var name:String = "New String"
+    // var name = "New String" -> type inference
+
+
+
+    println("Hello $userName your age is $age")
 }
