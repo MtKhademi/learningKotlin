a68899a34c7cbb7fc4ac8b5f189966c43558ccff
MT.Khademi <MT.Khademi@MPC10002236>
2024-12-02 07:54:20 +0330

    Data type

---
+++ learningKotlin/src/avasa/DataTypes.kt
@@ -0,0 +1,30 @@
+package avasa
+
+fun main() {
+    // this file is about data type in kotlin
+
+    /*
+
+    -- multi line comment
+
+    this file
+    is
+    about
+    kotlin data types
+
+     */
+
+
+    // var nameVariable:Type = value
+    var name = "SAM"
+    var family = "Nikzad"
+    var age:Int = 27
+
+//    name = 12 // error
+
+    var isStudent = false
+    isStudent = true
+
+    println("Is student : $isStudent")
+
+}
--- learningKotlin/src/avasa/MyVariables.kt
+++ learningKotlin/src/avasa/MyVariables.kt
@@ -1,2 +1,9 @@
 package avasa
 
+fun main() {
+    var Book = "Kotlin tutorial book from avasam"
+    var BookId = 1254
+
+    println("the book name is : " + Book)
+    println("the book id is : " + BookId)
+}
